8cacb843f20f38a5890daff452fc8adb43dd1569
Aaron Frily <thestreakcraft.official@gmail.com>
2026-05-28 22:05:42 +0200

    Creating ticket methods

--- src/main/java/ch/frily/xyzbot/ticketsystem/TicketController.java
+++ src/main/java/ch/frily/xyzbot/ticketsystem/TicketController.java
@@ -3,7 +3,9 @@ package ch.frily.xyzbot.ticketsystem;
 import ch.frily.xyzbot.utils.EnvKey;
 import ch.frily.xyzbot.utils.EnvResolver;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.requests.restaction.ChannelAction;
@@ -26,12 +28,26 @@ public class TicketController {
 
     public void createTicket(Ticket ticket) {
         Category ticketCategory = EnvResolver.getCategoryById(EnvKey.CATEGORY_TICKETS);
+
+        // Ticket settings
         ChannelAction<TextChannel> ticketCreation = ticketCategory.createTextChannel(resolveName(ticket));
         ticketCreation.addMemberPermissionOverride(ticket.getOwner().getIdLong(), ownerPermissions, null).queue();
+        ticketCreation.setTopic(ticket.getType().getLabel()).queue();
 
+        // Ticket content
         ticketCreation.queue(textChannel -> {
             log.info("Ticket done");
         });
+    }
+
+    public MessageEmbed createEmbed(Ticket ticket) {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle(ticket.getType().getLabel());
+        StringBuilder description = new StringBuilder();
+        description.append("Willkommen ").append(ticket.getOwner().getAsMention()).append("!");
+        description.append("\n");
+        description.append("Schildere dein Anliegen und das Team wird dir baldmöglich weiterhelfen.");
+        return embed.build();
     }
 
     public String resolveName(Ticket ticket) {
